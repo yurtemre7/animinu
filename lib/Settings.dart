17934a88c4764aa7308bb6d94de600bf26859bdd
Emre Yurtseven <emreyurtseven18@gmail.com>
2021-09-05 18:53:02 +0200

    added user info @ settings with username change

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -2,6 +2,7 @@ import 'package:animinu/components/Dividers.dart';
 import 'package:animinu/main.dart';
 import 'package:animinu/utilities.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings extends StatefulWidget {
@@ -16,7 +17,7 @@ class _SettingsState extends State<Settings> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: Text('Einstellungen'),
         centerTitle: true,
       ),
       body: SafeArea(
@@ -24,6 +25,67 @@ class _SettingsState extends State<Settings> {
           padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
           child: ListView(
             children: [
+              Consumer(
+                builder: (context, watch, child) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.person_pin_rounded),
+                        title: Text('${watch(username).state!}'),
+                        onTap: () {
+                          TextEditingController controller =
+                              TextEditingController(text: context.read(username).state);
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text('Username'),
+                              content: TextFormField(
+                                controller: controller,
+                                decoration: InputDecoration(
+                                  hintText: 'Username',
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  child: Text('Abbrechen'),
+                                  onPressed: () {
+                                    pop(context);
+                                  },
+                                ),
+                                ElevatedButton(
+                                  child: Text('Update'),
+                                  onPressed: () {
+                                    if (controller.text.isNotEmpty) {
+                                      // set username in database
+                                      final user = context.read(myUser).state;
+
+                                      database.reference().child(user!.uid).child('profile').update({
+                                        'name': controller.text.trim(),
+                                      });
+
+                                      context.read(username).state = controller.text.trim();
+                                    }
+                                    pop(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                      Dividers.horizontalDivider(),
+                      ListTile(
+                        trailing: Icon(Icons.email),
+                        title: Text('Deine E-Mail:'),
+                        subtitle: Text(
+                          watch(email).state!,
+                        ),
+                      ),
+                      Dividers.horizontalDivider(),
+                    ],
+                  );
+                },
+              ),
               ListTile(
                 leading: Icon(isDarkmode(context) ? Icons.light_mode : Icons.dark_mode),
                 title: Text('Gibt es einen ${isDarkmode(context) ? 'Lightmode' : 'Darkmode'}?'),
@@ -32,7 +94,7 @@ class _SettingsState extends State<Settings> {
               ),
               Dividers.horizontalDivider(),
               ListTile(
-                trailing: Icon(Icons.person),
+                trailing: Icon(Icons.info_outline),
                 title: Text('Wer hat diese App gemacht?'),
                 subtitle: Text('Emre Yurtseven.'),
                 onTap: () {
