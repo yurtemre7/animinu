03ab84419ca6162518db07c005fe441b6582968b
Emre Yurtseven <emreyurtseven18@gmail.com>
2023-12-29 23:25:22 +0100

    update packages

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yurtemre/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yurtemre/Code/Flutter Projects/animinu"
+export "FLUTTER_ROOT=C:\Users\emrey\Tools\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Programming\flutter projs\animinu"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
